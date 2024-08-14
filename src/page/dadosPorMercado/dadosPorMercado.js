6de10f1971525eb973efe219b72dcda8727c4c7b
Felipe Andrade <Felipe17.rezende@gmail.com>
2024-08-14 17:11:12 -0300

    teste

--- src/page/dadosPorMercado/dadosPorMercado.js
+++ src/page/dadosPorMercado/dadosPorMercado.js
@@ -7,7 +7,7 @@ import fotoCarne from '../../imagens/carne.png';
 function DadosPorMercado() {
     return (
         <>
-            <header className='cabecalho_titulo'>
+            <header className='cabecalho_titulo teste'>
                 <a href="/" className="cabecalho_link" aria-label="Voltar para o perfil">
                     <img src={iconeVoltar} alt="Voltar para o perfil" className="cabecalho_link" />
                 </a>
