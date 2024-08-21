54cf780b94fdc7ec6e64274bb4a185ecb321d650
Sauro4i20 <161897249+Sauro4i20@users.noreply.github.com>
2024-08-21 17:04:05 -0300

    login

--- src/page/login/index.js
+++ src/page/login/index.js
@@ -1,22 +1,41 @@
-import './style.css';
+import React, { useState } from "react";
+import "../login/login.css";
 
 function Login() {
+    const [isLogin, setIsLogin] = useState(true);
+
+    const toggleForm = () => {
+        setIsLogin(!isLogin);
+    };
+
     return (
-      <div className="container">
-        <h2>Login</h2>
-        <form>
-          <div>
-            <input id="username" name="username" type="text" placeholder="CPF" />
-          </div>
-          <div>
-            <input id="password" name="password" type="password" placeholder="Senha" />
-          </div>
-          <button type="submit">Login</button>
-          <a href="/forgot-password">Esqueceu sua senha?</a>
-        </form>
-      </div>
+        <div className="modal-container">
+            <div className="modal-content">
+                <h2>{isLogin ? "Login" : "Cadastro"}</h2>
+                <form>
+                    <div className="form-group">
+                        <input type="email" id="email" name="email" required placeholder="CPF:" />
+                    </div>
+                    <div className="form-group">
+                        <input type="password" id="password" name="password" required placeholder="Senha:" />
+                    </div>
+                    {!isLogin && (
+                        <div className="form-group">
+                            <input type="password" id="confirmPassword" name="confirmPassword" required placeholder="confirmar senha:" />
+                        </div>
+                    )}
+                </form>
+                <div className="button-login">
+
+                <button className="toggle-button" onClick={toggleForm}>
+                    {isLogin ? "Cadastre-se" : "Já tem uma conta? Faça login"}
+                </button>
+                    <button className="login-cadastro-button" type="submit">{isLogin ? "Login" : "cadastrar"}  </button>
+
+                </div>
+            </div>
+        </div>
     );
-  }
-  
-  export default Login;
-  
+}
+
+export default Login;
