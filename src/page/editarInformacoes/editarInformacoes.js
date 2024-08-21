9b21739b97733106bd65a909860a832e735703f5
Felipe Andrade <Felipe17.rezende@gmail.com>
2024-08-21 17:23:23 -0300

    alteração 21/08 - 17:23

--- src/page/editarInformacoes/editarInformacoes.js
+++ src/page/editarInformacoes/editarInformacoes.js
@@ -1,9 +1,9 @@
 import '../../assets/cabecalho.css'
 import iconeVoltar from '../../imagens/icon-voltar.svg';
 
-function EditarInformacoes(){
+function EditarInformacoes() {
 
-    return(
+    return (
 
         <>
 
@@ -11,14 +11,79 @@ function EditarInformacoes(){
 
                 <a href="/perfil">
 
-                    <img src={iconeVoltar} alt="Voltar para perfil"/>
+                    <img src={iconeVoltar} alt="Voltar para perfil" />
 
                 </a>
 
                 <h1 className="cabecalho_titulo">Editar Informações</h1>
 
             </header>
-        
+
+
+            <h2>Informações Pessoais</h2>
+
+            <form action="">
+
+                <label for="user">Nome:</label>
+                <input type="text" id="user" />
+
+                <label for="cpf">CPF:</label>
+                <input type="text" id="cpf" />
+
+                <label for="dataNascimento">Data de Nascimento:</label>
+                <input type="text" id="dataNascimento" />
+
+            </form>
+
+            <form action="">
+
+                <label for="senha">Senha:</label>
+                <input type="password" id="senha" />
+
+                <button>Redefinir senha</button>
+
+            </form>
+
+            <h2>Endereço</h2>
+
+            <form action="">
+
+                <label for="user">Nome:</label>
+                <input type="text" id="user" />
+
+                <label for="cpf">CPF:</label>
+                <input type="text" id="cpf" />
+
+                <label for="dataNascimento">Data de Nascimento:</label>
+                <input type="text" id="dataNascimento" />
+
+                <label for="senha">Senha:</label>
+                <input type="password" id="senha" />
+
+                <button>Redefinir senha</button>
+
+            </form>
+
+            <h2>Endereço</h2>
+            
+            <form action="">
+
+                <label for="user">Nome:</label>
+                <input type="text" id="user" />
+
+                <label for="cpf">CPF:</label>
+                <input type="text" id="cpf" />
+
+                <label for="dataNascimento">Data de Nascimento:</label>
+                <input type="text" id="dataNascimento" />
+
+                <label for="senha">Senha:</label>
+                <input type="password" id="senha" />
+
+                <button>Redefinir senha</button>
+
+            </form>
+
         </>
 
     )
