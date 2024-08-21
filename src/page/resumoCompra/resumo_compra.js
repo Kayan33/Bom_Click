a87f2357300fadb9edbb5cf6cc2b709f553358e5
Danrley Dimas <144742939+DanrleyDimasCN@users.noreply.github.com>
2024-08-21 17:14:23 -0300

    pagina resumo css em andamento

--- src/page/resumoCompra/resumo_compra.js
+++ src/page/resumoCompra/resumo_compra.js
@@ -9,15 +9,14 @@ import carne from '../../imagens/carne.png'
 import batata from '../../imagens/batata.png'
 import limao from '../../imagens/limao.png'
 import editarEndereco from '../../imagens/editar.svg'
+import '../../assets/carrossel.css'
 
 
 export default function ResumoCompra() {
   return(
     <div>
     <header className='cabecalho'>
-        <Link to='/'>
         <img src={iconeVoltar} alt="Voltar para Página Inicial" />
-        </Link>
         <h1 className='cabecalho_titulo'>Resumo da compra</h1>
       </header>
       <main>
@@ -25,48 +24,88 @@ export default function ResumoCompra() {
           <h2>Confira os produtos</h2>
           <img src={confiancaLogo} alt="Logo do supermercado confiança"/>
           <div className='scroll_conferir_produtos'>
-          <ul className="barraRolagem">
-                              <li>
-                                <div className='scroll_produto'>
-                                        <img src={carne} alt=" Imagem de Carne Vermelha" />
-                                        <p>Fraldinha Bovina Resfriada KG</p>
-                                        <div className='scroll_economia'>
-                                            <p>R$ 40,00</p>
-                                            <p>Melhor Preço!</p>
-                                        </div>
-                                </div>
-                            </li>
-                            <li> 
-                                <div className='scroll_produto'>
-                                        <img src={limao} alt="Imagem de limão Taiti" />
-                                        <p>Limão Taiti KG</p>
-                                        <div className='scroll_economia'>
-                                            <p>R$ 40,00</p>
-                                            <p>R$ 0,00</p>
-                                        </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className='scroll_produto'>
-                                        <img src={alho} alt="Imagem de alho" />
-                                        <p>Alho KG</p>
-                                        <div className='scroll_economia'>
-                                            <p>R$ 0,50</p>
-                                            <p>R$ 0,00</p>
-                                        </div>
-                                </div>
-                            </li>
-                            <li> 
-                                <div className='scroll_produto'>
-                                        <img src={batata} alt="Imagem de batata" />
-                                        <p>Batata KG</p>
-                                        <div className='scroll_economia'>
-                                            <p>R$ 40,00</p>
-                                            <p>R$ 5,00</p>
-                                        </div>
-                                </div>
-                            </li>
-                        </ul>
+
+          <ul className='secao_compras_produtos_lista barraRolagem'>
+
+<li className='secao_compras_produtos_lista_item'>
+
+    <article className='secao_compras_lista_item_produto'>
+
+        <img src={carne} alt="Fraldinha" className='secao_compras_lista_item_produto_imagem' />
+
+        <h3 className='secao_compras_lista_item_produto_titulo secao_compras_lista_item_produto_titulo'>Fraldinha Bovina Resfriada KG</h3>
+
+        <div className='secao_compras_lista_item_produto_valores'>
+
+            <h4 className='secao_compras_lista_item_produto_titulo secao_compras_lista_item_produto_titulo--preço'>R$40,00</h4>
+
+            <h4 className='secao_compras_lista_item_produto_titulo secao_compras_lista_item_produto_titulo--desconto secao_compras_lista_item_produto_titulo--amarelo'>R$40,00</h4>
+
+        </div>
+
+    </article>
+
+</li>
+
+<li className='secao_compras_produtos_lista_item'>
+
+    <article className='secao_compras_lista_item_produto'>
+
+        <img src={limao} alt="Fraldinha" className='secao_compras_lista_item_produto_imagem' />
+
+        <h3 className='secao_compras_lista_item_produto_titulo secao_compras_lista_item_produto_titulo'>Limao Taiti KG</h3>
+
+        <div className='secao_compras_lista_item_produto_valores'>
+
+            <h4 className='secao_compras_lista_item_produto_titulo secao_compras_lista_item_produto_titulo--preço'>R$10,00</h4>
+
+            <h4 className='secao_compras_lista_item_produto_titulo secao_compras_lista_item_produto_titulo--desconto secao_compras_lista_item_produto_titulo--amarelo'>R$40,00</h4>
+
+        </div>
+
+    </article>
+
+</li>
+<li className='secao_compras_produtos_lista_item'>
+
+    <article className='secao_compras_lista_item_produto'>
+
+        <img src={alho} alt="Fraldinha" className='secao_compras_lista_item_produto_imagem' />
+
+        <h3 className='secao_compras_lista_item_produto_titulo secao_compras_lista_item_produto_titulo'>Alho UN</h3>
+
+        <div className='secao_compras_lista_item_produto_valores'>
+
+            <h4 className='secao_compras_lista_item_produto_titulo secao_compras_lista_item_produto_titulo--preço'>R$0,50</h4>
+
+            <h4 className='secao_compras_lista_item_produto_titulo secao_compras_lista_item_produto_titulo--desconto secao_compras_lista_item_produto_titulo--amarelo'>R$40,00</h4>
+
+        </div>
+
+    </article>
+
+</li>
+<li className='secao_compras_produtos_lista_item'>
+
+    <article className='secao_compras_lista_item_produto'>
+
+        <img src={batata} alt="Fraldinha" className='secao_compras_lista_item_produto_imagem' />
+
+        <h3 className='secao_compras_lista_item_produto_titulo secao_compras_lista_item_produto_titulo'>Batata UN</h3>
+
+        <div className='secao_compras_lista_item_produto_valores'>
+
+            <h4 className='secao_compras_lista_item_produto_titulo secao_compras_lista_item_produto_titulo--preço'>R$5,00</h4>
+
+            <h4 className='secao_compras_lista_item_produto_titulo secao_compras_lista_item_produto_titulo--desconto secao_compras_lista_item_produto_titulo--amarelo'>R$1,00</h4>
+
+        </div>
+
+    </article>
+
+</li>
+
+</ul>
           </div>
         </section>
 
