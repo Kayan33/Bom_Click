91e576176e7ac78f536243d5fffb2544bb26d9ae
Sauro4i20 <161897249+Sauro4i20@users.noreply.github.com>
2024-08-23 14:44:51 -0300

    + input

--- src/page/login/index.js
+++ src/page/login/index.js
@@ -13,36 +13,49 @@ function Login() {
             <div className="modal-content">
                 <h2>{isLogin ? "Login" : "Cadastro"}</h2>
                 <form>
+                    {!isLogin &&(
+
                     <div className="form-group">
-                        <input type="email" id="email" name="email" required placeholder="CPF:" />
+                        <input type="text" id="nome" name="nome" required placeholder="Nome:" />
                     </div>
+                    )}
+                    <div className="form-group">
+                        <input type="text" id="cpf" name="cpf" required placeholder="CPF:" />
+                    </div>
+                    {!isLogin && (
+
+                        <div className="form-group">
+                            <input type="date" id="dataNascimento" name="dataNascimento" required />
+                        </div>
+                    )}
                     <div className="form-group">
                         <input type="password" id="password" name="password" required placeholder="Senha:" />
                     </div>
                     {!isLogin && (
                         <div className="form-group">
-                            <input type="password" id="confirmPassword" name="confirmPassword" required placeholder="confirmar senha:" />
+                            <input type="password" id="confirmPassword" name="confirmPassword" required placeholder="Confirmar Senha:" />
                         </div>
                     )}
                 </form>
+
                 <div className="button-login">
-                <div className='esqueci_senha'>
+                    <div className='esqueci_senha'>
 
-                    {isLogin && (
-                        <a className="special-button">
-                            Esqueci a senha
+                        {isLogin && (
+                            <a className="special-button">
+                                Esqueci a senha
+                            </a>
+                        )}
+                    </div>
+                    <div className="login">
+
+                        <a className="toggle-button" onClick={toggleForm}>
+                            {isLogin ? "Cadastre-se" : "Já tem uma conta? Faça login"}
                         </a>
-                    )}
-                </div>
-                <div className="login">
-
-                    <a className="toggle-button" onClick={toggleForm}>
-                        {isLogin ? "Cadastre-se" : "Já tem uma conta? Faça login"}
-                    </a>
-                    <button className="login-cadastro-button" type="submit">
-                        {isLogin ? "Login" : "Cadastrar"}
-                    </button>
-                </div>
+                        <button className="login-cadastro-button" type="submit">
+                            {isLogin ? "Login" : "Cadastrar"}
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
