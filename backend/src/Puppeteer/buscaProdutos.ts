c89edd2aedcbbec4d02252057e95b7ce2f38ec28
felipe <ferende.bentenisson@gmail.com>
2025-02-19 17:19:09 -0300

    alterações pastas

---
+++ backend/src/Puppeteer/buscaProdutos.ts
@@ -0,0 +1,10 @@
+import puppeteer from "puppeteer";
+
+ export async function buscaProdutos(){
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    
+ }
+
