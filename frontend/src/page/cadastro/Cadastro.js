75ce4e8a496e86a7919ada381337b39a1fe66950
Danrley Dimas <144742939+DanrleyDimasCN@users.noreply.github.com>
2025-03-26 14:44:47 -0300

    26/03

--- frontend/src/page/cadastro/Cadastro.js
+++ frontend/src/page/cadastro/Cadastro.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Link from 'react-router-dom'
 import "../cadastro/cadastro.css"
 
 export default function Cadastro() {
--- frontend/src/page/login/index.js
+++ frontend/src/page/login/index.js
@@ -2,58 +2,44 @@ import React, { useState } from "react";
 import "../login/login.css";
 
 function Login() {
-    const [isLogin, setIsLogin] = useState(true);
 
-    const toggleForm = () => {
-        setIsLogin(!isLogin);
-    };
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
 
     return (
         <div className="modal-container">
             <div className="modal-content">
-                <h2>{isLogin ? "Login" : "Cadastro"}</h2>
+                <h2>Login</h2>
                 <modal>
                     <form>
-                        {!isLogin &&(
                         <div className="form-group">
-                            <input type="text" id="nome" name="nome" required placeholder="Nome:" />
+                            <input
+                                type="text"
+                                required placeholder="Email:"
+                                value={email}
+                            />
                         </div>
-                        )}
                         <div className="form-group">
-                            <input type="text" id="cpf" name="cpf" required placeholder="CPF:" />
+                            <input
+                                type="password"
+                                required placeholder="Senha:"
+                                value={senha}
+                            />
                         </div>
-                        {!isLogin && (
-                            <div className="form-group">
-                                <input type="date" id="dataNascimento" name="dataNascimento" required />
-                            </div>
-                        )}
-                        <div className="form-group">
-                            <input type="password" id="password" name="password" required placeholder="Senha:" />
-                        </div>
-                        {!isLogin && (
-                            <div className="form-group">
-                                <input type="password" id="confirmPassword" name="confirmPassword" required placeholder="Confirmar Senha:" />
-                            </div>
-                        )}
                     </form>
                 </modal>
-
                 <div className="button-login">
                     <div className='esqueci_senha'>
-
-                        {isLogin && (
-                            <a className="special-button">
-                                Esqueci a senha
-                            </a>
-                        )}
+                        <a className="special-button">
+                            Esqueci a senha
+                        </a>
                     </div>
                     <div className="login">
-
-                        <a className="toggle-button" onClick={toggleForm}>
-                            {isLogin ? "Cadastre-se" : "Já tem uma conta? Faça login"}
+                        <a>
+                            <p>Cadastre-se</p>
                         </a>
                         <button className="login-cadastro-button" type="submit">
-                            {isLogin ? "Login" : "Cadastrar"}
+                            <p>Login</p>
                         </button>
                     </div>
                 </div>
