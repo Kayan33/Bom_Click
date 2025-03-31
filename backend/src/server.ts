c6876dec20970d72b63bc41d8008b5ce2b81c2bb
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-03-31 15:53:33 -0300

    feat: adicionar documentação Swagger para CadastroUsuarios

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,15 @@ import express, { Request, Response, NextFunction  } from "express";
 import 'express-async-errors';
 import cors from 'cors'; 
 import router from './routes';
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "./swagger.json"
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(router)
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDoc))
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
