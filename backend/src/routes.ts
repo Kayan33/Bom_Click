1379d8f3e8408e9d0822ada60c0907c47fb57f5f
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-03-31 15:23:47 -0300

    eat: adicionar cadastro de usuários com criptografia de senha

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import Puppeteer from './Puppeteer/buscaProdutos';
+import ControllerPost from './Controllers/ControllersPost/ControllersPost';
 
 const router = Router();
 
 router.get('/BuscarProdutos', new Puppeteer().buscaProdutosConfiança)
+router.post('/CadastroUsuarios', new ControllerPost().cadastro_Usuarios)
 
 export default router;
