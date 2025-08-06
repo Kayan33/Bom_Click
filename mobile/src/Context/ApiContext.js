1c9bff39494830602ba3f43b496dd935e8b3beb9
Danrley Dimas <144742939+DanrleyDimasCN@users.noreply.github.com>
2025-08-06 17:33:51 -0300

    Modal cep Feito

--- mobile/src/Context/ApiContext.js
+++ mobile/src/Context/ApiContext.js
@@ -6,13 +6,8 @@ export const ApiContext = createContext();
 export default function ApiProvider({ children }) {
 
     const apiBack = axios.create({
-<<<<<<< HEAD
         baseURL: 'http://192.168.56.1:3333'
         // baseURL: 'http://192.168.1.195:3333'
-=======
-        // baseURL: 'http://192.168.56.1:3333'
-        baseURL: 'https://kayanpereira.com.br:21025'
->>>>>>> 8957e5317340e4dd298801806673703975a6385a
     });
 
     const buscaPromocoes = async () =>{
